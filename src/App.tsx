fab2ee7f41f405a3021ff80d6aed9101002d968d
Shashank Nayak <shashank96.nayak@gmail.com>
2025-12-24 15:47:33 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Canvas from "./components/Canvas";
+import Header from "./components/Header";
+import Toolbar from "./components/Toolbar";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Canvas></Canvas>
+      <Toolbar></Toolbar>
+    </>
+  );
+}
+
+export default App;
